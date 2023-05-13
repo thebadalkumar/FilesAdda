372440d50daf64cd1e16aaff15ad26a9902fbbc0
Badal Kumar <101865301+thebadalkumar@users.noreply.github.com>
2023-05-13 23:42:16 +0530

    Update settings.py

--- FilesAdda/settings.py
+++ FilesAdda/settings.py
@@ -86,8 +86,9 @@ DATABASES = {
 }
 
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.config(),
 }
 
 # Password validation
